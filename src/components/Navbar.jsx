1330babfdf9bf0ef86b979c9976517e696eb2930
azmain-siam <siamhossain2700@gmail.com>
2024-06-03 07:40:24 +0600

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,154 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { HiMenuAlt2 } from "react-icons/hi";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleShowSideNav = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+
+  const navlinks = (
+    <>
+      <li className="block p-1 antialiased leading-normal">
+        <Link className="flex items-center">Donation Requests</Link>
+      </li>
+      <li className="block p-1 antialiased leading-normal">
+        <Link className="flex items-center">Blog</Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="grid w-full place-items-center rounded-lg p-6 lg:overflow-visible">
+      <div className="-m-6 max-h-[768px] w-[calc(100%+48px)]">
+        <nav className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 bg-white border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+          <div className="flex max-w-[1440px] mx-auto items-center justify-between text-blue-gray-900">
+            {/* <----- Hamburger Icon ------> */}
+            <div className="flex gap-5 items-center">
+              <div className="lg:hidden">
+                <button
+                  onClick={handleShowSideNav}
+                  className="navbar-burger flex items-center "
+                >
+                  <HiMenuAlt2 size={21}/>
+                </button>
+              </div>
+              <Link
+                to={"/"}
+                className="mr-4 block cursor-pointer  text-2xl font-bold leading-relaxed text-inherit antialiased"
+              >
+                BloodBond
+              </Link>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="hidden mr-4 lg:block">
+                <ul className="flex flex-col font-medium gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+                  {navlinks}
+                </ul>
+              </div>
+              <div className="flex items-center gap-x-1">
+                <button
+                  className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-semibold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+                  type="button"
+                >
+                  <span>Sign in</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      {/* Sidenavbar */}
+      <div className={`navbar-menu relative z-50 ${!open && "hidden"}`}>
+        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+          <div className="flex justify-between items-center mb-8">
+            <Link to={"/"} className="text-2xl font-bold ">
+              BloodBond
+            </Link>
+            <button onClick={handleShowSideNav} className="navbar-close">
+              <svg
+                className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div>
+            <ul>
+              <li className="mb-1">
+                <NavLink
+                  to={"/"}
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
+                  href="#"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
+                  href="#"
+                >
+                  About Us
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
+                  href="#"
+                >
+                  Services
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
+                  href="#"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li className="mb-1">
+                <a
+                  className="block p-4 text-sm font-semibold text-gray-400 hover:bg-blue-50 hover:text-blue-600 rounded"
+                  href="#"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-auto">
+            <div className="pt-6">
+              <a
+                className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold bg-gray-50 hover:bg-gray-100 rounded-xl"
+                href="#"
+              >
+                Sign in
+              </a>
+              <a
+                className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl"
+                href="#"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      <h3>Hello</h3>
+    </div>
+  );
+};
+export default Home;
