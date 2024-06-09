a7bcb1693e7d963e207a4cc9d847fc74f35d7038
azmain-siam <siamhossain2700@gmail.com>
2024-06-09 09:35:00 +0600

    JWT applied

--- src/components/Dashboard/Users.jsx
+++ src/components/Dashboard/Users.jsx
@@ -1,4 +1,23 @@
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
 const Users = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users", {
+        headers: {
+          authorization: `${localStorage.getItem("token")}`,
+        },
+      });
+      return res.data;
+    },
+  });
+
+  console.log(users);
+
   return (
     <div>
       <h3 className="text-xl">Users Page</h3>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import Helmet from "react-helmet";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Home | BloodBond</title>
+      </Helmet>
       <h3>Hello</h3>
     </div>
   );
