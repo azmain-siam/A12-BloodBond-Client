7a6bba68368a0e95f095979cdedea0b9215cf543
azmain-siam <siamhossain2700@gmail.com>
2024-06-12 02:27:16 +0600

    Finished Content management page

--- src/components/Dashboard/ContentManagement.jsx
+++ src/components/Dashboard/ContentManagement.jsx
@@ -60,19 +60,21 @@ const ContentManagement = () => {
     }
   };
 
-  if (blogsLoading) {
+  if (blogsLoading || isLoading) {
     return <LoadingBars />;
   }
 
   return (
     <div className="font-poppins">
-      <div className="mb-4 text-3xl font-extrabold leading-none text-gray-900 md:text-4xl flex justify-between items-center">
+      <div className="mb-6 text-3xl font-extrabold leading-none text-gray-900 md:text-4xl flex justify-between items-center">
         Content Management
-        <Link to={"/dashboard/content-management/add-blog"}>
-          <button className="btn btn-sm btn-neutral text-base tracking-normal	font-medium text-white py-2 px-4 h-full">
-            Add Blog
-          </button>
-        </Link>
+        {role === "admin" && (
+          <Link to={"/dashboard/content-management/add-blog"}>
+            <button className="btn btn-sm btn-neutral text-base tracking-normal	font-medium text-white py-2 px-4 h-full">
+              Add Blog
+            </button>
+          </Link>
+        )}
       </div>
 
       <div className="relative overflow-auto h-screen ">
