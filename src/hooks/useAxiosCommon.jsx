a29eee26583c2c50344bf2bcc1f7693d6ac83c04
azmain-siam <siamhossain2700@gmail.com>
2024-06-12 19:03:48 +0600

    Server changed

--- src/hooks/useAxiosCommon.jsx
+++ src/hooks/useAxiosCommon.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosCommon = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bloodbond-nine.vercel.app",
 });
 const useAxiosCommon = () => {
   return axiosCommon;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
 export const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bloodbond-nine.vercel.app",
   // withCredentials: true,
 });
 const useAxiosSecure = () => {
